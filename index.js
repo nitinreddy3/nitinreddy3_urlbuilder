eeced6ac78ff528f1f77eca72e682f45a767d56b
Nitin Reddy <reddynitin3@gmail.com>
2020-10-05 22:19:38 +0530

    Created a REST API for execises and users

---
+++ index.js
@@ -0,0 +1,17 @@
+exports.buildRestUrl = function (restUrl, urlParams, queryParams) {
+  let url = restUrl;
+  let query = '';
+  url = restUrl.replace(/{([^{}]*)}/g, (subString, index) => {
+    var current = urlParams[index];
+    return /string|number/.test(typeof current) ? current : subString;
+  });
+  if (Object.keys(queryParams).length) {
+    url = `${url}?`;
+    for (let key in queryParams) {
+      if (queryParams.hasOwnProperty(key)) {
+        query = `${query}${key}=${(queryParams[key])}&`;
+      }
+    }
+  }
+  return `${url}${query}`;
+};
